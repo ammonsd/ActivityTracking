ba73cd561358afdfb09e65ae9cc12de73183799b
Dean Ammons <36488173+ammonsd@users.noreply.github.com>
2025-10-31 19:24:16 -0400

    Remove temporary SQL script for admin user password update and retrieval.

--- temp-fix-admin.sql
+++
@@ -1,8 +0,0 @@
-﻿UPDATE users 
-SET userpassword = '$2a$10$EH4s9bxxtZynR/d1/ZSBAORnaVw4JRN3abKR2uC51Fe8LnuBG.9yK',
-    forcepasswordupdate = false
-WHERE username = 'admin';
-
-SELECT username, LEFT(userpassword, 20) as pwd_hash, userrole, enabled 
-FROM users 
-WHERE username = 'admin';
