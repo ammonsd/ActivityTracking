1dd34d28d28a2f9f92c43e53e4481e78a178a679
Dean Ammons <36488173+ammonsd@users.noreply.github.com>
2026-02-01 05:58:46 -0500

    Update Content Security Policy to allow Cloudflare Insights

--- src/main/java/com/ammons/taskactivity/config/SecurityConfig.java
+++ src/main/java/com/ammons/taskactivity/config/SecurityConfig.java
@@ -140,11 +140,11 @@ public class SecurityConfig {
                                                 // loading
                                                 .contentSecurityPolicy(csp -> csp.policyDirectives(
                                                                 "default-src 'self'; "
-                                                                                + "script-src 'self' 'unsafe-inline' 'unsafe-eval'; "
+                                                                                + "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com; "
                                                                                 + "style-src 'self' 'unsafe-inline'; "
                                                                                 + "img-src 'self' data:; "
                                                                                 + "font-src 'self' data:; "
-                                                                                + "connect-src 'self'"))
+                                                                                + "connect-src 'self' https://cloudflareinsights.com"))
                                                 // HTTP Strict Transport Security: Force HTTPS
                                                 .httpStrictTransportSecurity(hsts -> hsts
                                                                 .includeSubDomains(true)
@@ -462,11 +462,11 @@ public class SecurityConfig {
                                                 // (TODO: migrate to nonces)
                                                 .contentSecurityPolicy(csp -> csp.policyDirectives(
                                                                 "default-src 'self'; "
-                                                                                + "script-src 'self' 'unsafe-inline' 'unsafe-eval'; "
+                                                                                + "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com; "
                                                                                 + "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
                                                                                 + "img-src 'self' data: https:; "
                                                                                 + "font-src 'self' data: https://fonts.gstatic.com; "
-                                                                                + "connect-src 'self'; "
+                                                                                + "connect-src 'self' https://cloudflareinsights.com; "
                                                                                 + "frame-ancestors 'none'"))
                                                 // HTTP Strict Transport Security - Force HTTPS
                                                 .httpStrictTransportSecurity(hsts -> hsts
