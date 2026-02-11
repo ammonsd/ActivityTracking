940e0015f32d74160fed5748e90d19bfad839670
Dean Ammons <36488173+ammonsd@users.noreply.github.com>
2026-02-11 10:52:36 -0500

    Phase 4 changes

--- frontend-react/src/components/userManagement/ChangePasswordDialog.tsx
+++ frontend-react/src/components/userManagement/ChangePasswordDialog.tsx
@@ -209,6 +209,7 @@ export const ChangePasswordDialog: React.FC<ChangePasswordDialogProps> = ({
                                 characters
                             </li>
                             <li>Not be the same as the current password</li>
+                            <li>Cannot match any of your previous 5 passwords</li>
                         </ul>
                     </Alert>
 
